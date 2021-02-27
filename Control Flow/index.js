88a439b56f209c1c03952de418dfb362257e8a3f
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-02-27 17:25:12 +0330

    Condition:if/else

---
+++ Control Flow/index.js
@@ -0,0 +1,13 @@
+/** section1 - If/else */
+
+let hour = 20;
+
+if(hour >=6 && hour < 12){
+    console.log("Good Morning");
+}
+else if (hour >= 12 && hour < 18){
+    console.log("Good Afternoon");
+}
+else{
+    console.log("Good Evening");
+}
