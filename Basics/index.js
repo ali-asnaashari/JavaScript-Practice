d5f7bfd74ffb7d77b6ea3a3076bd8eb36424eed1
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-02-27 14:28:39 +0330

    update index.js - variables

--- Basics/index.js
+++ Basics/index.js
@@ -1,2 +1,28 @@
 /** ---------------------------------- section1 My First Js Code ----------------------------------- **/
 console.log("Hello world");
+
+/** ---------------------------------- section2 Variables ----------------------------------------- **/
+
+/**
+ - Previously , in the old days , before ES6 , we use the var keyword to declare variable.
+ - now use the let keyword.
+ - by default variable in js their value is undefined.
+ -
+ **/
+
+/** part1 **/
+let name;
+console.log(name);
+
+/** part2 **/
+let name1 = 'ali';
+console.log(name1);
+
+/** Rules
+ 1. can not be a reserved key word
+ 2. should be meaningful
+ 3. can not start with number exp:1name
+ 4. can not contain space or hyohen(-)
+ 5. use camel notation
+ 6. are case-sensitive
+ */
