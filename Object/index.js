2ef7b7d026a9beda9f86b497b6c8402cdc94c93a
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-02-27 19:49:02 +0330

    Constructor Functions

--- Object/index.js
+++ Object/index.js
@@ -24,6 +24,8 @@ circle.draw();
 
 // Factory Functions ---> to create simply object
 
+// in Factory Functions we use Camel Notation Like oneTwoThreeFour
+
 // In modern JS, if our key and value are the same ,
 // we can make our code shorter by removing value
 // and simply adding the key
@@ -40,3 +42,30 @@ function createCircle(radius){
 const cricle1 = createCircle(1);
 console.log(cricle1);
 console.log(cricle1.draw());
+
+/** section3 - Constructor Functions **/
+
+/** Note:
+ * Learning another Pattern to Create Function
+ * in Constructor Functions we use Pascal Notation Like OneTwoThreeFour
+ * this ----> Reference to the object
+ */
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function (){
+        console.log('draw');
+    }
+    return this;
+}
+
+const circle1 = new Circle(1);
+
+
+
+
+
+
+
+
+
